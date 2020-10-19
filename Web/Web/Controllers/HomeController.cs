6deba6eb685e9fc7a1319c0ce7580d8d3dbec59f
Mohammad Hossein Karami <mhkarami1997@gmail.com>
2020-10-19 22:06:23 +0330

    add new method and view to home controller

--- Web/Web/Controllers/HomeController.cs
+++ Web/Web/Controllers/HomeController.cs
@@ -1,26 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
+using Web.Models;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Web.Models;
+using Messaging.InterfacesConstants.Commands;
+using Messaging.InterfacesConstants.Constants;
+using MassTransit;
+using Web.ViewModels;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IBusControl _busControl;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IBusControl busControl)
         {
             _logger = logger;
+            _busControl = busControl;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult RegisterOrder()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterOrder(OrderViewModel model)
+        {
+            var memory = new MemoryStream();
+
+            await using (var uploadedFile = model.File.OpenReadStream())
+            {
+                await uploadedFile.CopyToAsync(memory);
+            }
+
+            model.ImageData = memory.ToArray();
+            model.OrderId = Guid.NewGuid();
+            model.PictureUrl = model.File.FileName;
+
+            var sendToUri = new Uri($"{RabbitMqMassTransitConstants.RabbitMqUri}" +
+                                    $"{RabbitMqMassTransitConstants.RegisterOrderCommandQueue}");
+
+            var endPoint = await _busControl.GetSendEndpoint(sendToUri);
+            
+            await endPoint.Send<IRegisterOrderCommand>(
+                new
+                {
+                    model.OrderId,
+                    model.UserEmail,
+                    model.ImageData,
+                    model.PictureUrl
+                });
+            
+            ViewData["OrderId"] = model.OrderId;
+            
+            return View("Thanks");
         }
 
         public IActionResult Privacy()
