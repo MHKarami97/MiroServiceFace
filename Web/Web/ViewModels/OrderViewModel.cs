f581ff17ac0e5bcf15c42d707145eb45c93b594d
Mohammad Hossein Karami <mhkarami1997@gmail.com>
2020-10-29 10:44:37 +0330

    add OrderManagementController

--- Web/Web/ViewModels/OrderViewModel.cs
+++ Web/Web/ViewModels/OrderViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Http;
 using System.ComponentModel.DataAnnotations;
 
@@ -6,21 +7,23 @@ namespace Web.ViewModels
 {
     public class OrderViewModel
     {
-        [Display(Name = "Order Id")]
+        [Display(Name ="Order Id")]
         public Guid OrderId { get; set; }
 
-        [Display(Name = "Email")]
+        [Display(Name ="Email")]
         public string UserEmail { get; set; }
 
-        [Display(Name = "Image File")]
+        [Display(Name ="Image File")]
         public IFormFile File { get; set; }
 
-        [Display(Name = "PictureUrl")]
+        [Display(Name ="PictureUrl")]
         public string PictureUrl { get; set; }
 
-        [Display(Name = "Order Status")]
-        public string StatusString { get; set; }
+        [Display(Name ="Order Status")]
+        public string Status { get; set; }
 
         public byte[] ImageData { get; set; }
+        public string ImageString { get; set; }
+        public List<OrderDetailViewModel> OrderDetails { get; set; }
     }
 }
